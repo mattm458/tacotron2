076d4050154c484f146d1bdda4f3e458fe069048
Matt <mattm458@gmail.com>
2022-01-31 15:44:46 -0500

    put speaker embeddings in main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import csv
 import pandas as pd
 import yaml
 from pytorch_lightning import Trainer
+from torch import nn
 
 from datasets.tts_dataloader import TTSDataLoader
 from datasets.tts_dataset import TTSDataset
-from model.tacotron2 import Tacotron2
 from model.gst import GST
+from model.tacotron2 import Tacotron2
 
 
 def load_dataset(filepath, config):
@@ -58,11 +59,16 @@ if __name__ == "__main__":
     )
 
     # gst = GST()
+    speaker_embeddings = nn.Embedding(
+        config["extensions"]["speaker_embeddings"]["num_speakers"],
+        config["extensions"]["speaker_embeddings"]["embedding_dim"],
+    )
+    speaker_embeddings_dim = config["extensions"]["speaker_embeddings"]["embedding_dim"]
 
     tacotron2 = Tacotron2(
         lr=config["training"]["lr"],
         weight_decay=config["training"]["weight_decay"],
-        num_chars=len(config["preprocessing"]["valid_chars"])+1,
+        num_chars=len(config["preprocessing"]["valid_chars"]) + 1,
         encoder_kernel_size=config["encoder"]["encoder_kernel_size"],
         num_mels=config["audio"]["num_mels"],
         char_embedding_dim=config["encoder"]["char_embedding_dim"],
@@ -74,6 +80,8 @@ if __name__ == "__main__":
         dropout=config["tacotron2"]["dropout"],
         # gst=gst,
         # gst_dim=256,
+        speaker_embeddings=speaker_embeddings,
+        speaker_embeddings_dim=speaker_embeddings_dim,
     )
 
     trainer = Trainer(
@@ -87,5 +95,5 @@ if __name__ == "__main__":
         tacotron2,
         train_dataloaders=train_dataloader,
         val_dataloaders=val_dataloader,
-        #ckpt_path="lightning_logs/version_55/checkpoints/epoch=81-step=16317.ckpt",
+        # ckpt_path="lightning_logs/version_55/checkpoints/epoch=81-step=16317.ckpt",
     )
