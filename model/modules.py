786d69a884624da58ba1eb1e2901d4fd0b3e047d
Matt <mattm458@gmail.com>
2022-03-07 12:41:52 -0500

    add XavierConv2d module

--- model/modules.py
+++ model/modules.py
@@ -39,6 +39,29 @@ class XavierConv1d(nn.Module):
         return conv_signal
 
 
+class XavierConv2d(nn.Module):
+    """An extension of the PyTorch Conv1d class that applies Xavier weight initialization"""
+
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,
+                 padding=None, dilation=1, bias=True, nonlinearity='linear'):
+        super().__init__()
+        if padding is None:
+            assert(kernel_size % 2 == 1)
+            padding = int(dilation * (kernel_size - 1) / 2)
+
+        self.conv = nn.Conv2d(in_channels, out_channels,
+                                    kernel_size=kernel_size, stride=stride,
+                                    padding=padding, dilation=dilation,
+                                    bias=bias)
+
+        nn.init.xavier_uniform_(
+            self.conv.weight, gain=nn.init.calculate_gain(nonlinearity))
+
+    def forward(self, signal):
+        conv_signal = self.conv(signal)
+        return conv_signal
+
+
 class AlwaysDropout(nn.Module):
     """An extension of the PyTorch Dropout class that applies dropout during inference"""
     def __init__(self, p):
