321981892bb20b25264bc71da657bff50523e034
Matt <mattm458@gmail.com>
2023-05-28 12:42:31 -0400

    train improvements

--- run/train.py
+++ run/train.py
@@ -2,10 +2,12 @@ import csv
 import datetime
 import os
 from os import path
+from typing import Optional
 
 import pandas as pd
 import torch
 from lightning import Trainer
+from lightning.pytorch.callbacks import EarlyStopping, LearningRateMonitor
 from lightning.pytorch.loggers import TensorBoardLogger
 
 from datasets.tts_dataloader import TTSDataLoader
@@ -17,9 +19,18 @@ def do_train(
     dataset_config: dict,
     training_config: dict,
     model_config: dict,
+    extensions_config: dict,
     device: int,
     speech_dir: str,
+    results_dir: Optional[str] = None,
+    resume_ckpt: Optional[str] = None,
 ):
+    if results_dir is None:
+        results_dir = f"results_{training_config['name']} {datetime.datetime.now()}"
+        os.mkdir(results_dir)
+
+    cache_dir = path.join(results_dir, "mel_cache")
+
     train_df = pd.read_csv(
         dataset_config["train"], delimiter="|", quoting=csv.QUOTE_NONE, engine="c"
     )
@@ -27,16 +38,34 @@ def do_train(
         dataset_config["val"], delimiter="|", quoting=csv.QUOTE_NONE, engine="c"
     )
 
+    train_features = (
+        train_df[extensions_config["controls"]["features"]].values.tolist()
+        if extensions_config["controls"]["active"]
+        else None
+    )
+
     train_dataset = TTSDataset(
         filenames=train_df.wav,
         texts=train_df.text,
+        speaker_ids=train_df.speaker_id if extensions_config["speaker_id"] else None,
+        features=train_features,
         base_dir=speech_dir,
+        cache_dir=cache_dir,
         **dataset_config["preprocessing"],
+    )
+
+    val_features = (
+        val_df[extensions_config["controls"]["features"]].values.tolist()
+        if extensions_config["controls"]["active"]
+        else None
     )
     val_dataset = TTSDataset(
         filenames=val_df.wav,
         texts=val_df.text,
+        speaker_ids=val_df.speaker_id if extensions_config["speaker_id"] else None,
+        features=val_features,
         base_dir=speech_dir,
+        cache_dir=cache_dir,
         **dataset_config["preprocessing"],
     )
 
@@ -60,9 +89,6 @@ def do_train(
         drop_last=False,
     )
 
-    results_dir = f"results_{training_config['name']} {datetime.datetime.now()}"
-    os.mkdir(results_dir)
-
     torch.set_float32_matmul_precision(training_config["float32_matmul_precision"])
 
     logger = TensorBoardLogger(
@@ -84,12 +110,14 @@ def do_train(
         accelerator="gpu",
         precision=training_config["precision"],
         gradient_clip_val=1.0,
-        max_epochs=training_config["max_epochs"],
         check_val_every_n_epoch=1,
+        callbacks=[LearningRateMonitor(logging_interval="step")],
+        **training_config["args"],
     )
 
     trainer.fit(
         model,
         train_dataloaders=train_dataloader,
         val_dataloaders=val_dataloader,
+        ckpt_path=resume_ckpt,
     )
